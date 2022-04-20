e39536cc1726b9923abb41268d71ba3e7cdc29ec
desak intan <100027338+desakintan@users.noreply.github.com>
2022-04-20 15:30:46 +0800

    Add files via upload

---
+++ Kepsek.java
@@ -0,0 +1,14 @@
+package com.kuliah;
+
+public class Kepsek {
+    String NIP;
+    String nama;
+    String jk;
+
+    void display(){
+        System.out.println("Nama : " + this.nama);
+        System.out.println("NIP : " + this.NIP);
+        System.out.println("Jenis Kelamin : " + this.jk);
+        System.out.println("\n");
+    }
+}
